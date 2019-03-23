36c2847207313f11149d10cf131683617ad06ddc
manjusa <thizismanju@gmail.com>
2019-03-23 12:04:41 +1100

    first draft

---
+++ sampleJson.js
@@ -0,0 +1,55 @@
+{
+    "items":[
+        {
+            "order":0,
+            "name" : "cat"
+        },
+        {
+            "order":1,
+            "name" : "dog"
+        }
+        ,
+        {
+            "order":2,
+            "name" : "elephant"
+        }
+        ,	{
+            "order":3,
+            "name" : "tiger"
+        }
+        ,
+        {
+            "order":4,
+            "name" : "cow"
+        },
+        {
+            "order":5,
+            "name" : "Giraffe"
+        },
+        {
+            "order":6,
+            "name" : "Gorillas"
+        },
+        {
+            "order":7,
+            "name" : "Horses"
+        },
+        {
+            "order":8,
+            "name" : "Hippos"
+        },
+        {
+            "order":9,
+            "name" : "Kangaroo"
+        }
+        ,
+        {
+            "order":10,
+            "name" : "Wolf"
+        },
+         {
+            "order":11,
+            "name" : "Wsdolf"
+        }
+    ]
+}
